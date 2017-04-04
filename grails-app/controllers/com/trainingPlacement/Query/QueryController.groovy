ac0d1b4731b5f9af21d1225bed5fff9c532e3cda
akanksha241096 <akankshadewangan96@gmail.com>
2017-04-04 15:33:19 +0530

    dicussuion forum

--- grails-app/controllers/com/trainingPlacement/Query/QueryController.groovy
+++ grails-app/controllers/com/trainingPlacement/Query/QueryController.groovy
@@ -1,13 +1,35 @@
 package com.trainingPlacement.Query
 
-
+import com.trainingPlacement.Discussion.Discussion
+import com.trainingPlacement.SpringSecurity.User
 import grails.plugin.springsecurity.annotation.Secured
 
-
 class QueryController {
-     @Secured(['ROLE_ADMIN'])
+
+    def springSecurityService
+
+    @Secured(['permitAll'])
     def index() {
 
-        render "inside query"
+    }
+
+    def save() {
+        def authorinstance=User.get(springSecurityService.currentUser.id)
+        println authorinstance
+        Discussion discussinstance= new Discussion([
+                queryTitle:params.querytitle,
+                queryBody:params.querybody,
+                dateCreated:new Date(),
+                lastUpdate:new Date(),
+                author:authorinstance
+        ])
+
+        println discussinstance.queryTitle
+        println discussinstance.queryBody
+        println discussinstance.dateCreated
+        println discussinstance.lastUpdate
+        println discussinstance.author
+        discussinstance.save()
+        redirect(controller:"query",action:"index")
     }
 }
